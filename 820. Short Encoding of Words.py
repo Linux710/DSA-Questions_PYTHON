27bebb82ed0d2f851136d7c63cd62ac4f1fe21d4
anmolthakur843 <anmolthakur843@gmail.com>
2022-07-30 23:20:32 +0530

    Update 820. Short Encoding of Words.py

--- 820. Short Encoding of Words.py
+++ 820. Short Encoding of Words.py
@@ -5,5 +5,5 @@ class Solution:
         for x in words: # iterating over each words
             if x+'#' not in ans:   # if that particular word is not present in our reference string then we are adding it to our answer
                 ans+=x+'#'
-        return len(ans)
+        return len(ans)#ans
         
